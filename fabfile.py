d0ad543df419329e168e5d2c5443604165ac9e02
Anthony Pesce <anthonyjpesce@gmail.com>
2013-10-28 12:26:05 -0700

    updated Ruby install for the latest version of Chef

--- fabfile.py
+++ fabfile.py
@@ -85,12 +85,12 @@ def install_chef():
     """
     # Install dependencies
     sudo('apt-get update', pty=True)
-    sudo('apt-get install -y git-core rubygems ruby ruby-dev', pty=True)
+    sudo('apt-get install -y git-core rubygems1.9.1 ruby1.9.1 build-essential ruby1.9.1-dev', pty=True)
     # Screw ruby docs.
     sudo("echo 'gem: --no-ri --no-rdoc' > /root/.gemrc")
     sudo("echo 'gem: --no-ri --no-rdoc' > /home/ubuntu/.gemrc")
     # Install Chef
-    sudo('gem install chef', pty=True)
+    sudo('gem install --no-ri --no-rdoc chef', pty=True)
 
 
 def cook():
