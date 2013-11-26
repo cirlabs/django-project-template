749030dc3224261a02b6fcd16f802f43fd363f98
aboutaaron <aaron.colby.williams@gmail.com>
2013-11-25 18:08:08 -0800

    global settings

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -54,7 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+ROOT_URLCONF = 'urls'
 
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
