39008c47ece9b5d5e1ba580a50e3c66382ff7067
aboutaaron <aaron.colby.williams@gmail.com>
2013-12-05 11:53:10 -0800

    removed unused files and updated rserver command to run grunt

--- project_name/apps/base/management/commands/rserver.py
+++ project_name/apps/base/management/commands/rserver.py
@@ -6,14 +6,14 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = "Runs `compass --watch` and `collectstatic` before launching Django's own runserver"
+    help = "Runs `grunt default` before launching Django's own runserver"
 
     def handle(self, *args, **options):
-        self.start_compass_watch()
+        self.grunt()
 
         # `interactive = False` is the undocumented equivalent of the '--no-input' flag,
         # which we can't use in a management command. See http://stackoverflow.com/a/9999429/8438
-        management.call_command('collectstatic', interactive = False)
+        # management.call_command('collectstatic', interactive = False)
 
         # Allow specifying an IP:port string, if provided
         if args:
@@ -21,14 +21,14 @@ class Command(BaseCommand):
         else:
             management.call_command('runserver')
 
-    def start_compass_watch(self):
-        self.stdout.write(self.style.NOTICE(">>> Compass is watching %r") % settings.COMPASS_INPUT_PATH + "\n" )
-        self.stdout.write(self.style.NOTICE(">>> Generated CSS is at %r") % settings.COMPASS_OUTPUT_PATH + "\n" )
-        self.compass_process = subprocess.Popen(
-            ['compass watch --sass-dir %s --css-dir %s' % (settings.COMPASS_INPUT_PATH, settings.COMPASS_OUTPUT_PATH)],
+    def grunt(self):
+        # self.stdout.write(self.style.NOTICE(">>> Compass is watching %r") % settings.COMPASS_INPUT_PATH + "\n" )
+        # self.stdout.write(self.style.NOTICE(">>> Generated CSS is at %r") % settings.COMPASS_OUTPUT_PATH + "\n" )
+        subprocess.Popen(
+            ['cd %s && grunt' % settings.BASE_DIR],
             shell=True,
             stdin=subprocess.PIPE,
             stdout=self.stdout,
             stderr=self.stderr,
         )
-        self.stdout.write(self.style.NOTICE(">>> Compass watch process on pid %r" % self.compass_process.pid) + "\n")
+        # self.stdout.write(self.style.NOTICE(">>> Compass watch process on pid %r" % self.compass_process.pid) + "\n")
