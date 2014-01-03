7b09fdf8c80bacde05b6558199e15c03ad46195f
aboutaaron <aaron.colby.williams@gmail.com>
2014-01-03 14:14:44 -0800

    removed BayCitizen utils

--- project_name/apps/core/utils.py
+++ project_name/apps/core/utils.py
@@ -6,3 +6,5 @@ def fully_qualify_if_url(request, url):
     url = "%s" % url  # cast to string for safety
     protocol = get_current_protocol(request) if url.startswith("//") else ''
     return "%s%s" % (protocol, url)
+
+
