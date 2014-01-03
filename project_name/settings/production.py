c7cafafbaf1605dbfa477e4ff729c3a8b18eb500
aboutaaron <aaron.colby.williams@gmail.com>
2014-01-02 16:42:32 -0800

    added conditional for heroku deploys for databses. Changed procfile for the correct config. Addresses #10 and #11

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -2,6 +2,8 @@ from common import *
 
 DEBUG = False
 
+HEROKU_DEPLOY = False
+
 # enable asset compression on production
 # it defaults to the opposite of debug
 COMPRESS_ENABLED = True
@@ -12,17 +14,26 @@ ALLOWED_HOSTS = ['{{ project_name }}.apps.cironline.org']
 if DEBUG:
     MIDDLEWARE_CLASSES.append('apps.core.middleware.LoginRequiredMiddleware')
 
-# Production database info
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': '{{ project_name }}',
-        'PORT': '6432',  # PGBouncer port
-        'HOST': 'some-ec2-instance', # ec2-54-244-141-139.us-west-2.compute.amazonaws.com
-        'USER': '{{ project_name }}',
-        'PASSWORD': 'secret'
+# Databases
+if HEROKU_DEPLOY:
+    import dj_database_url # Heroku only
+    # Heroku Specific
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+else:
+    # Production database info
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'NAME': '{{ project_name }}',
+            'PORT': '6432',  # PGBouncer port
+            'HOST': 'ec2-54-244-141-139.us-west-2.compute.amazonaws.com',
+            'USER': '{{ project_name }}',
+            'PASSWORD': 'secret'
+        }
     }
-}
 
 # Static
 STATIC_URL = 'http://media.apps.cironline.org/va_opiates_public/site_media/'
