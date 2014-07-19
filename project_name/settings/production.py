9aa7c431fadf4e7c4568eca30c685944af2fe573
Aaron Williams <aaron.colby.williams@gmail.com>
2014-07-18 17:22:51 -0700

    added production details

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -2,58 +2,22 @@ from common import *
 
 DEBUG = False
 
-HEROKU_DEPLOY = False
-
 # enable asset compression on production
 # it defaults to the opposite of debug
 COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_OUTPUT_DIR = 'prod'
+
+# Django Bakery
+BUILD_DIR = os.path.join(BASE_DIR, 'build')
+BAKERY_VIEWS = (
+    # Django Bakery Views go here
+    # '{{ project_name }}.apps.some_news_app.views.SomeNewsAppView',
+)
+AWS_BUCKET_NAME = 'apps.cironline.org'
 
 # Defaults to project name. Add correct one
-ALLOWED_HOSTS = ['{{ project_name }}.apps.cironline.org']
-
-if DEBUG:
-    MIDDLEWARE_CLASSES.append('apps.core.middleware.LoginRequiredMiddleware')
-
-# Databases
-if HEROKU_DEPLOY:
-    import dj_database_url # Heroku only
-    # Heroku Specific
-    DATABASES['default'] =  dj_database_url.config()
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-else:
-    # Production database info
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.contrib.gis.db.backends.postgis',
-            'NAME': '{{ project_name }}',
-            'PORT': '6432',  # PGBouncer port
-            'HOST': 'ec2-54-244-141-139.us-west-2.compute.amazonaws.com',
-            'USER': '{{ project_name }}',
-            'PASSWORD': 'secret'
-        }
-    }
+#ALLOWED_HOSTS = ['apps.cironline.org']
 
 # Static
-STATIC_URL = 'http://media.apps.cironline.org/va_opiates_public/site_media/'
-
-# GEOS paths for GeoDjango and GDAL. Configured for our particular Heroku setup.
-GEOS_LIBRARY_PATH = '/app/.geodjango/geos/lib/libgeos_c.so'
-GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
-
-# Caching
-CACHE_MIDDLEWARE_SECONDS = 90 * 60  # 90 minutes
-
-os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '').replace(',', ';')
-os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
-os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-        'LOCATION': os.environ.get('MEMCACHIER_SERVERS', '').replace(',', ';'),
-        'TIMEOUT': 500,
-        'BINARY': True,
-    }
-}
+STATIC_URL = 'http://media.apps.cironline.org/'
