534ecf416cfe0f99d8c55d08b24a40918717c1e7
Aaron Williams <aaron.colby.williams@gmail.com>
2014-07-18 17:07:32 -0700

    changed spacing and added new commands from previous project fabfiles

--- fabfile.py
+++ fabfile.py
@@ -22,153 +22,179 @@ sys.path.append(pwd)
 # Environment
 
 def production():
-  """
-  Work on production environment
-  """
-  env.settings = 'production'
+    """
+    Work on production environment
+    """
+    env.settings = 'production'
 
 # Setup and install
 
 def setup():
-  """
-  Fetch required dependencies and setup the front-end
-  """
-  print ("Do you need to install the project's node.js dependencies (Yo, Bower, Grunt)? <'y' or 'n'>")
-  while True:
-    answer = raw_input("> ")
+    """
+    Fetch required dependencies and setup the front-end
+    """
+    print ("Do you need to install the project's node.js dependencies (Yo, Bower, Grunt)? <'y' or 'n'>")
+    while True:
+        answer = raw_input("> ")
 
-    if (answer.upper() == 'Y'):
-      get_node_libs()
-      break
+        if (answer.upper() == 'Y'):
+          get_node_libs()
+          break
 
-    elif (answer.upper() == 'N'):
-      print ("Skipping dependency install")
-      break
+        elif (answer.upper() == 'N'):
+          print ("Skipping dependency install")
+          break
 
-    else:
-      print ("Error: you did not answer 'y' or 'n'")
+        else:
+          print ("Error: you did not answer 'y' or 'n'")
 
-  yo()
-  createdb()
+        yo()
+        createdb()
 
 def get_node_libs():
-  """
-  Install Node.js for Mac OS X or Ubuntu Linux
-  """
-  print ("Are you running this on Mac OS X or Ubuntu Linux? <Answer 'Mac' or 'Linux'>")
-  platform = raw_input("> ")
-  if (platform.upper() == 'MAC'):
-    local('npm install -g yo grunt-cli bower generator-newsapp')
-  elif (platform.upper() == 'LINUX'):
-    local('sudo npm install -g yo grunt-cli bower generator-newsapp')
-  else:
-    print ("Error: you did not answer 'Mac' or 'Linux'")
+    """
+    Install Node.js for Mac OS X or Ubuntu Linux
+    """
+    print ("Are you running this on Mac OS X or Ubuntu Linux? <Answer 'Mac' or 'Linux'>")
+    platform = raw_input("> ")
+
+    if (platform.upper() == 'MAC'):
+        local('npm install -g yo grunt-cli bower generator-newsapp')
+    elif (platform.upper() == 'LINUX'):
+        local('sudo npm install -g yo grunt-cli bower generator-newsapp')
+    else:
+        print ("Error: you did not answer 'Mac' or 'Linux'")
+
     get_node_libs()
 
 def yo():
-  """
-  Run yeoman generator to scaffold front-end dependencies
-  """
-  local('cd {0} && yo newsapp'.format(project_name))
+    """
+    Run yeoman generator to scaffold front-end dependencies
+    """
+    local('cd {0} && yo newsapp'.format(project_name))
+
 
 def install_node():
-  """
-  Install node.js for Mac OS X or Ubuntu Linux
-  """
-  print ("Installing Node, eh? Mac or Linux? <answer 'Mac' or 'Linux'>")
+    """
+    Install node.js for Mac OS X or Ubuntu Linux
+    """
+    print ("Installing Node, eh? Mac or Linux? <answer 'Mac' or 'Linux'>")
 
-  answer = raw_input("> ")
-  if (answer.upper() == 'MAC'):
-    print ("Installing Node with Homebrew. Is Homebrew installed?")
-    while True:
-      answer = raw_input("> ")
-      if (answer.upper() == 'Y'):
-
-        local("brew install node")
-        break
-
-      elif (answer.upper() == 'N'):
-        local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"')
-        local("brew install node")
-        break
-
-      else:
-        print ("You did not answer 'Y' or 'N'.")
-
-  elif (answer.upper() == 'LINUX'):
-    local('wget http://nodejs.org/dist/v0.10.25/node-v0.10.25.tar.gz')
-    local('tar -xvzf node-v0.10.25.tar.gz')
-    local('cd node-v0.10.25')
-    local('mkdir ~/.node')
-    local('./configure --prefix=~/.node')
-    local('make')
-    local('make install')
-    local('echo "export PATH=~/.node/bin:${PATH}" >> ~/.zshrc')
-    local('echo "export PATH=~/.node/bin:${PATH}" >> ~/.bashrc')
-    # this may happen automatically so check first
-    local('echo "export NODE_PATH=$NODE_PATH:/$HOME/.node/lib/node_modules" >> ~/.zshrc && source ~/.zshrc')
-    local('echo "export NODE_PATH=$NODE_PATH:/$HOME/.node/lib/node_modules" >> ~/.bashrc && source ~/.bashrc')
-    local('cd ..')
-    local('rm -rf node-v0.10.25')
-    local('rm node-v0.10.25.tar.gz')
-    
-    print "Node.js installed"
-
-  else:
-    print("You didn't answer Mac or Linux")
-    install_node()
-
-def rs(port=8000):
-  """
-  Start development server and grunt tasks. Optionally, specify port
-  """
-  local("python manage.py rserver 0.0.0.0:%s" % port)
-
-def grunt():
-  """
-  Run grunt tasks installed from Yeoman generator
-  """
-  local('cd {{ project_name }} && grunt')
-
-def startapp(app_name=''):
-  """
-  Create django app
-  """
-  local("python manage.py startapp {0}".format(app_name))
-  local("mv {0} {1}/apps/".format(app_name, project_name))
-  print("\nHEADS UP! Make sure you add '{0}.apps.{1}' to INSTALLED_APPS in settings/common.py\n".format(project_name, app_name))
-
-def createdb():
-  """
-  Creates local database for project
-  """
-  local('createdb {0}'.format(project_name))
-  local('echo "CREATE EXTENSION postgis;" | psql {0}'.format(project_name))
-
-def dropdb():
-  """
-  drops local database for project
-  """
-  local('echo "DROP DATABASE {0};" | psql postgres'.format(project_name))
-
-def destroy():
-  """
-  destoys the database and django project. Be careful!
-  """
-  print("You are about to mothball this entire project. Sure you want to do that? <enter 'Y' or 'N'>")
-  while True:
     answer = raw_input("> ")
-    if (answer.upper() == 'Y'):
-      dropdb()
-      local('cd .. && rm -rf {0}'.format(project_name))
-      break
+    if (answer.upper() == 'MAC'):
+        print ("Installing Node with Homebrew. Is Homebrew installed?")
+        while True:
+            answer = raw_input("> ")
+            if (answer.upper() == 'Y'):
 
-    elif (answer.upper() == 'N'):
-      print("cancelling destory")
-      break
+                local("brew install node")
+                break
+
+            elif (answer.upper() == 'N'):
+                local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"')
+                local("brew install node")
+                break
+
+            else:
+                print ("You did not answer 'Y' or 'N'.")
+
+    elif (answer.upper() == 'LINUX'):
+        local('wget http://nodejs.org/dist/v0.10.28/node-v0.10.28.tar.gz')
+        local('tar -xvzf node-v0.10.28.tar.gz')
+        local('cd node-v0.10.28')
+        local('mkdir ~/.node')
+        local('./configure --prefix=~/.node')
+        local('make')
+        local('make install')
+        # local('echo "export PATH=~/.node/bin:${PATH}" >> ~/.zshrc')
+        # local('echo "export PATH=~/.node/bin:${PATH}" >> ~/.bashrc')
+        # # this may happen automatically so check first
+        # local('echo "export NODE_PATH=$NODE_PATH:/$HOME/.node/lib/node_modules" >> ~/.zshrc && source ~/.zshrc')
+        # local('echo "export NODE_PATH=$NODE_PATH:/$HOME/.node/lib/node_modules" >> ~/.bashrc && source ~/.bashrc')
+        local('cd ..')
+        local('rm -rf node-v0.10.28')
+        local('rm node-v0.10.28.tar.gz')
+
+        print "Node.js installed"
 
     else:
-      print("You didn't answer 'Y' or 'N'")
+        print("You didn't answer Mac or Linux")
+        install_node()
+
+def rs(port=8000):
+    """
+    Start development server and grunt tasks. Optionally, specify port
+    """
+    local("python manage.py rserver 0.0.0.0:%s" % port)
+
+def grunt():
+    """
+    Run grunt tasks installed from Yeoman generator
+    """
+    local('cd {{ project_name }} && grunt')
+
+def startapp(app_name=''):
+    """
+    Create django app
+    """
+    local("python manage.py startapp {0}".format(app_name))
+    local("mv {0} {1}/apps/".format(app_name, project_name))
+    print("\nHEADS UP! Make sure you add '{0}.apps.{1}' to INSTALLED_APPS in settings/common.py\n".format(project_name, app_name))
+
+def dumpdata(app_name=''):
+    """
+    Dump the data of an app in json format and store it in the fixtures directory
+    """
+    local("python manage.py dumpdata {0} > fixtures/{1}.json".format(app_name, app_name))
+
+def loaddata(app_name=''):
+    """
+    Dump the data of an app in json format and store it in the fixtures directory
+    """
+    if app_name is not '':
+        fixtures_dir = os.path.join(settings.ROOT_DIR, app_name, 'fixtures', "{0}.json".format(app_name))
+        local("python manage.py loaddata {0}".format(fixtures_dir))
+
+    else:
+        print "please specify an app name"
+
+def createdb():
+    """
+    Creates local database for project
+    """
+    local('createdb {0}'.format(project_name))
+    local('echo "CREATE EXTENSION postgis;" | psql {0}'.format(project_name))
+
+def dropdb():
+    """
+    drops local database for project
+    """
+    local('echo "DROP DATABASE {0};" | psql postgres'.format(project_name))
+
+def clear(app_name, model_name):
+    """
+    Remove a model from an application database
+    """
+    local("echo 'DROP TABLE %s_%s;' | psql caform700" % (app_name, model_name))
+
+def destroy():
+    """
+    destoys the database and django project. Be careful!
+    """
+    print("You are about to mothball this entire project. Sure you want to do that? <enter 'Y' or 'N'>")
+    while True:
+        answer = raw_input("> ")
+        if (answer.upper() == 'Y'):
+            dropdb()
+            local('cd .. && rm -rf {0}'.format(project_name))
+            break
+
+        elif (answer.upper() == 'N'):
+            print("cancelling destory")
+            break
+
+        else:
+            print("You didn't answer 'Y' or 'N'")
 
 # Static media
 # This should all run after a grunt task runs to collect the deps used
@@ -179,13 +205,41 @@ def gzip_assets():
     """
     local("cd {0}; python ./gzip_assets.py".format(pwd))
 
+def grunt_build():
+    """Execute grunt build for any cleanup that needs to happen before deploying to s3"""
+    local('cd {{ project_name }} && grunt build')
+
 def deploy_to_s3():
     """
     Deploy the latest project site media to S3.
     """
-    local('s3cmd -P --add-header=Content-encoding:gzip --guess-mime-type --rexclude-from={0}/s3exclude sync {1} s3://{2}/{3}/{4}/'.format(localpath, gzip_path, s3_bucket, project_name, site_media_prefix))
+    local('s3cmd -P --add-header=Content-encoding:gzip --guess-mime-type --rexclude-from={0}/s3exclude sync {1} s3://{2}/{3}/'.format(localpath, gzip_path, s3_bucket, project_name))
 
-def deploy_static():
-    local("python ./{0}/manage.py collectstatic".format(project_name))
+def publish():
+    """publish build from django bakery to s3"""
+    local('s3cmd -P sync /home/aaron/Code/{{ project_name }}/{{ project_name }}/build/ s3://apps.cironline.org/hacienda/')
+
+def build():
+    """shortcut for django bakery build command"""
+    local('python manage.py build --skip-static --settings={{ project_name }}.settings.production')
+
+def unbuild():
+    """shortcut for django bakery unbuild command"""
+    local('python manage.py unbuild --settings={{ project_name }}.settings.production')
+
+def compress():
+    """shortcut for django compressor offline compression command"""
+    local('python manage.py compress --settings={{ project_name }}.settings.production')
+
+def reset():
+    """delete all the deploy code"""
+    local('cd {{ project_name }} && rm -rf static && rm -rf gzip && rm -rf build')
+
+def deploy():
+    reset()
+    compress()
+    build()
     gzip_assets()
+    grunt_build()
     deploy_to_s3()
+    publish()
