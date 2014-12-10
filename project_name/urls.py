f656c14385ba9845f8cff2988455d8f2de216b5a
Aaron Williams <aaron.colby.williams@gmail.com>
2014-12-10 15:54:31 -0800

    remove default url so the project can run without the yo generator. For #36

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Admin
     (r'^admin/', admin.site.urls),
-
-    # Project URLs go here
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='base'),
-
 )
