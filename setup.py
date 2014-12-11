04bea54929223ea38b78469edc7f17f54f957f61
Aaron Williams <aaron.colby.williams@gmail.com>
2014-12-11 15:11:35 -0800

    travis.yml setup

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            INSTALLED_APPS=(
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.admin',
+                'lib',
+            )
+        )
+        from django.core.management import call_command
+        call_command('test', 'lib')
+
+
+setup(
+    name='django-project-template',
+    cmdclass={'test': TestCommand}
+)
