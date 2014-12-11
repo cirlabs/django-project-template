6378c65bfb208843450be75976bdfb6421606b23
Aaron Williams <aaron.colby.williams@gmail.com>
2014-12-11 15:27:03 -0800

    pep8 and pyflakes

--- lib/gzip_assets.py
+++ lib/gzip_assets.py
@@ -1,21 +1,24 @@
 #!/bin/env python
-
 import os
 import gzip
 import shutil
+
 
 class FakeTime:
     def time(self):
         return 1261130520.0
 
 # Hack to override gzip's time implementation
-# See: http://stackoverflow.com/questions/264224/setting-the-gzip-timestamp-from-python
+# http://stackoverflow.com/a/264303/868724
 gzip.time = FakeTime()
 
 project_dir = '{{ project_name }}'
 
 shutil.rmtree(os.path.join(project_dir, 'gzip'), ignore_errors=True)
-shutil.copytree(os.path.join(project_dir, 'static'), os.path.join(project_dir, 'gzip/static'))
+shutil.copytree(
+    os.path.join(project_dir, 'static'),
+    os.path.join(project_dir, 'gzip/static')
+)
 
 for path, dirs, files in os.walk(os.path.join(project_dir, 'gzip/static')):
     for filename in files:
@@ -26,4 +29,4 @@ for path, dirs, files in os.walk(os.path.join(project_dir, 'gzip/static')):
         f_in.close()
         f_out = gzip.open(file_path, 'wb')
         f_out.writelines(contents)
-        f_out.close();
+        f_out.close()
