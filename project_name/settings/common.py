2fa95e433b3380ae1162827f40cb8c2d5f9a17af
Aaron Williams <aaron.colby.williams@gmail.com>
2014-12-10 16:11:08 -0800

    added static_root. Fixes #27

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/assets/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
