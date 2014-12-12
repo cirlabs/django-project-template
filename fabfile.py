2f21ffdac92c7c6c74de459672f63c42e2dda48b
Aaron Williams <aaron.colby.williams@gmail.com>
2014-12-11 16:12:26 -0800

    pep8 and pyflakes cleanup of fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,58 +1,27 @@
-from __future__ import with_statement
-
 import os
-import sys
-import boto
-import random
-from os.path import expanduser
-from boto.ec2.connection import EC2Connection
 
-from fabric.api import *
+from fabric.api import local
 from fabric.contrib import django
 
-django.project('{{project_name}}')
-from {{project_name}}.settings import common as settings
-from {{project_name}}.settings.production import AWS_BUCKET_NAME
 
-# vars
+project_name = '{0}'.format('{{ project_name }}')
+project_settings = project_name + '.settings'
+django.settings_module(project_settings)
+from django.conf import settings
+
+
 pwd = os.path.dirname(__file__)
-project_name = "{{ project_name }}"
 gzip_path = '{0}/{1}/gzip/static/'.format(pwd, project_name)
-site_media_prefix = "site_media"
-production_domain = 'apps.cironline.org' # prompt to define this
-s3_bucket = AWS_BUCKET_NAME
-media_s3_bucket = 'media-apps-cironline-org'
+
 s3_directory = project_name
+s3_bucket = settings.AWS_BUCKET_NAME
+media_s3_bucket = 'media-apps-cironline-org'
+site_media_prefix = "site_media"
+production_domain = 'apps.cironline.org'
 
-verbose_production_name = 'hired-guns' # what you want to call it when it goes live
+# what you want to call it when it goes live
+verbose_production_name = 'hired-guns'
 
-
-sys.path.append(pwd)
-
-
-# Setup and install
-
-def setup():
-    """
-    Fetch required dependencies and setup the front-end
-    """
-    print ("Do you need to install the project's node.js dependencies (Yo, Bower, Grunt)? <'y' or 'n'>")
-    while True:
-        answer = raw_input("> ")
-
-        if (answer.upper() == 'Y'):
-          get_node_libs()
-          break
-
-        elif (answer.upper() == 'N'):
-          print ("Skipping dependency install")
-          break
-
-        else:
-          print ("Error: you did not answer 'y' or 'n'")
-
-    yo()
-    createdb()
 
 def rs(port=8000):
     """
@@ -60,11 +29,13 @@ def rs(port=8000):
     """
     local("python manage.py rserver 0.0.0.0:%s" % port)
 
+
 def grunt():
     """
     Run grunt tasks installed from Yeoman generator
     """
     local('cd {{ project_name }} && grunt')
+
 
 def sh():
     """
@@ -72,30 +43,45 @@ def sh():
     """
     local('python manage.py shell_plus')
 
+
 def startapp(app_name=''):
     """
     Create django app
     """
     local("python manage.py startapp {0}".format(app_name))
     local("mv {0} {1}/apps/".format(app_name, project_name))
-    print("\nHEADS UP! Make sure you add '{0}.apps.{1}' to INSTALLED_APPS in settings/common.py\n".format(project_name, app_name))
+    print("\nHEADS UP! Make sure you add '{0}.apps.{1}' to \
+        INSTALLED_APPS in settings/common.py\n".format(project_name, app_name))
+
 
 def dumpdata(app_name=''):
     """
-    Dump the data of an app in json format and store it in the fixtures directory
+    Dump the data of an app in json format
+    and store it in the fixtures directory
     """
-    local("python manage.py dumpdata {0} > fixtures/{1}.json".format(app_name, app_name))
+    local("python manage.py dumpdata {0} > fixtures/{1}.json".format(
+        app_name, app_name
+    ))
+
 
 def loaddata(app_name=''):
     """
-    Dump the data of an app in json format and store it in the fixtures directory
+    load the data of an app in json format
+    and store it in the fixtures directory
     """
     if app_name is not '':
-        fixtures_dir = os.path.join(settings.ROOT_DIR, app_name, 'fixtures', "{0}.json".format(app_name))
+        fixtures_dir = os.path.join(
+            settings.ROOT_DIR,
+            app_name,
+            'fixtures',
+            "{0}.json".format(app_name)
+        )
+
         local("python manage.py loaddata {0}".format(fixtures_dir))
 
     else:
         print "please specify an app name"
+
 
 def createdb():
     """
@@ -104,7 +90,10 @@ def createdb():
     local('createdb {0}'.format(project_name))
 
     if settings.USE_POSTGIS:
-        local('echo "CREATE EXTENSION postgis;" | psql {0}'.format(project_name))
+        local('echo "CREATE EXTENSION postgis;" | psql {0}'.format(
+            project_name
+        ))
+
 
 def dropdb():
     """
@@ -112,17 +101,22 @@ def dropdb():
     """
     local('echo "DROP DATABASE {0};" | psql postgres'.format(project_name))
 
+
 def clear(app_name, model_name):
     """
     Remove a model from an application database
     """
-    local("echo 'DROP TABLE %s_%s;' | psql {{project_name}}" % (app_name, model_name))
+    local("echo 'DROP TABLE {0}_{1};' | psql {{project_name}}".format(
+        app_name, model_name
+    ))
+
 
 def destroy():
     """
     destoys the database and django project. Be careful!
     """
-    print("You are about to mothball this entire project. Sure you want to do that? <enter 'Y' or 'N'>")
+    print("You are about to mothball this entire project. \
+        Sure you want to do that? <enter 'Y' or 'N'>")
     while True:
         answer = raw_input("> ")
         if (answer.upper() == 'Y'):
@@ -139,6 +133,8 @@ def destroy():
 
 # Static media
 # This should all run after a grunt task runs to collect the deps used
+
+
 def gzip_assets():
     """
     GZips every file in the assets directory and places the new file
@@ -146,35 +142,54 @@ def gzip_assets():
     """
     local("cd {0}; python ./gzip_assets.py".format(pwd))
 
+
 def grunt_build():
-    """Execute grunt build for any cleanup that needs to happen before deploying to s3"""
+    """
+    Execute grunt build for any cleanup that
+    needs to happen before deploying to s3
+    """
     local('cd {{ project_name }} && grunt build')
+
 
 def deploy_to_s3():
     """
     Deploy the latest project site media to S3.
     """
-    local('s3cmd -P --add-header=Content-encoding:gzip --guess-mime-type --rexclude-from={0}/s3exclude sync {1} s3://{2}/{3}/'.format(localpath, gzip_path, s3_bucket, project_name))
+    local('s3cmd -P --add-header=Content-encoding:gzip \
+        --guess-mime-type --rexclude-from={0}/s3exclude sync {1} \
+        s3://{2}/{3}/'.format(pwd, gzip_path, media_s3_bucket, s3_directory))
+
 
 def publish():
     """publish build from django bakery to s3"""
-    local('s3cmd -P sync /home/aaron/Code/{{ project_name }}/{{ project_name }}/build/ s3://apps.cironline.org/hacienda/')
+    local('s3cmd -P sync \
+        /home/aaron/Code/{{ project_name }}/{{ project_name }}/build/ \
+        s3://apps.cironline.org/hacienda/')
+
 
 def build():
     """shortcut for django bakery build command"""
-    local('python manage.py build --skip-static --settings={{ project_name }}.settings.production')
+    local('python manage.py build \
+        --skip-static --settings={{ project_name }}.settings.production')
+
 
 def unbuild():
     """shortcut for django bakery unbuild command"""
-    local('python manage.py unbuild --settings={{ project_name }}.settings.production')
+    local('python manage.py unbuild \
+        --settings={{ project_name }}.settings.production')
+
 
 def compress():
     """shortcut for django compressor offline compression command"""
-    local('python manage.py compress --settings={{ project_name }}.settings.production')
+    local('python manage.py compress \
+        --settings={{ project_name }}.settings.production')
+
 
 def reset():
     """delete all the deploy code"""
-    local('cd {{ project_name }} && rm -rf static && rm -rf gzip && rm -rf build')
+    local('cd {{ project_name }} && \
+        rm -rf static && rm -rf gzip && rm -rf build')
+
 
 def deploy():
     reset()
