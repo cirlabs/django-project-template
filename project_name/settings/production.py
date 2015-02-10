014cb10894c91dad0a77645a572d2e367f3548bc
Aaron Williams <aaron.colby.williams@gmail.com>
2015-02-09 16:21:31 -0800

    removed unnecessary comments. Included staging server

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -2,8 +2,6 @@ from common import *
 
 DEBUG = False
 
-# enable asset compression on production
-# it defaults to the opposite of debug
 COMPRESS_ENABLED = True
 COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
@@ -13,7 +11,6 @@ COMPRESS_CSS_FILTERS = [
 COMPRESS_OFFLINE = True
 COMPRESS_OUTPUT_DIR = 'prod'
 
-# Django Bakery
 BUILD_DIR = os.path.join(BASE_DIR, 'build')
 BAKERY_VIEWS = (
     # Django Bakery Views go here
@@ -22,8 +19,4 @@ BAKERY_VIEWS = (
 AWS_STAGING_BUCKET_NAME = 'apps-staging-cironline-org'
 AWS_BUCKET_NAME = 'apps-cironline-org'
 
-# Defaults to project name. Add correct one
-#ALLOWED_HOSTS = ['apps.cironline.org']
-
-# Static
-STATIC_URL = '//s3-us-west-1.amazonaws.com/media-apps-cironline-org/{{ project_name }}/'
+STATIC_URL = "//s3-us-west-1.amazonaws.com/media-apps-cironline-org/{{ project_name }}/"
